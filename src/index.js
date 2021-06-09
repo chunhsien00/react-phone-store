2be1746329665cf54b70ec9cae2b67d16568edf3
tom <minieyeshsien00@gmail.com>
2021-06-09 12:50:57 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ProductProvider} from './context'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
